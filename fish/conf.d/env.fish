905312d6bdeb381c9af399caa2a7939ccd09442d
AyimanyMooncat <ayimanymooncat@proton.me>
2024-05-07 23:43:45 -0600

    [ADD] Python RC

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -7,10 +7,44 @@ set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_STATE_HOME  "$HOME/.local/state"
 set -gx XDG_CACHE_HOME  "$HOME/.cache"
 
+set -gx XDG_DESKTOP_DIR     "$HOME/desktop"
+set -gx XDG_DOCUMENTS_DIR   "$HOME/documents"
+set -gx XDG_DOWNLOAD_DIR    "$HOME/downloads"
+set -gx XDG_MUSIC_DIR       "$HOME/music"
+set -gx XDG_PICTURES_DIR    "$HOME/pictures"
+set -gx XDG_PUBLICSHARE_DIR "$HOME/public"
+set -gx XDG_TEMPLATES_DIR   "$HOME/templates"
+set -gx XDG_VIDEOS_DIR      "$HOME/videos"
+
+mkdir -p \
+	"$XDG_DATA_HOME" 	\
+	"$XDG_CONFIG_HOME" 	\
+	"$XDG_STATE_HOME" 	\
+	"$XDG_CACHE_HOME" 	\
+				\
+	"$XDG_DESKTOP_DIR" 	\
+	"$XDG_DOCUMENTS_DIR" 	\
+	"$XDG_DOWNLOAD_DIR" 	\
+	"$XDG_MUSIC_DIR" 	\
+	"$XDG_PICTURES_DIR" 	\
+	"$XDG_PUBLICSHARE_DIR" 	\
+	"$XDG_TEMPLATES_DIR" 	\
+	"$XDG_VIDEOS_DIR"
+
+set -gx XDG_EXT_PROJECTS_HOME 	"$HOME/projects"
+set -gx XDG_EXT_SCREENSHOTS_DIR	"$XDG_PICTURES_DIR/screenshots"
+
+mkdir -p \
+	"$MCS_SCREENSHOTS_DIR"
+
 set -gx EDITOR "nvim"
 set -gx VISUAL "nvim"
 
 set -gx CARGO_HOME  "$XDG_DATA_HOME/cargo"
 set -gx RUSTUP_HOME "$XDG_DATA_HOME/rustup"
 set -gx GOPATH      "$XDG_DATA_HOME/go"
+
+set -gx PYTHONSTARTUP "$XDG_CONFIG_HOME/python/rc.py"
+
+set -gx GRIM_DEFAULT_DIR "$XDG_EXT_SCREENSHOTS_DIR"
 
---
+++ python/rc.py
@@ -0,0 +1,31 @@
+def is_vanilla() -> bool:
+    import sys
+    return not hasattr(__builtins__, "__IPYTHON__") and "bpython" not in sys.argv[0]
+
+def setup_history():
+    import os
+    import atexit
+    import readline
+    from pathlib import Path
+
+    if state_home := os.environ.get("XDG_STATE_HOME"):
+        state_home = Path(state_home)
+    else:
+        state_home = Path.home() / ".local" / "state"
+
+    if not state_home.exists():
+        os.mkdir(state_home)
+
+    history: Path = state_home / "python_history"
+
+    if not history.exists():
+        file = open(history, "w")
+        file.close()
+
+    readline.read_history_file(str(history))
+    atexit.register(readline.write_history_file, str(history))
+
+if is_vanilla():
+    setup_history()
+
+
